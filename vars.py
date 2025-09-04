be1852b01b99387a217207d98355eb5ad0fd3b58
938377 <beyondlimits032@gmail.com>
2025-09-04 13:46:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,10 +2,11 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from os import environ
+from os import environment 
 
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
 
 
